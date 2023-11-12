68b56824fd48b7cc785a5e98ee83ef04d6a0f500
suxatcode <nnamegah21@gmx.de>
2023-11-12 20:45:59 +0800

    refactor: integration test to allow multiple queries

--- internal/app/server_integration_test.go
+++ internal/app/server_integration_test.go
@@ -45,54 +45,76 @@ const (
 }`
 )
 
+type graphqlQuery struct {
+	Query     string                 `json:"query"`
+	Variables map[string]interface{} `json:"variables,omitempty"`
+}
+
+type queryAndResult struct {
+	Payload  *graphqlQuery
+	Expected string
+}
+
 func TestGraphQLHandlers(t *testing.T) {
 	for _, test := range []struct {
-		Name     string
-		Payload  interface{}
-		Expected string
+		Name          string
+		QuerySequence []queryAndResult
 	}{
 		{
 			Name: "query: graph",
-			Payload: &struct {
-				Query string `json:"query"`
-			}{
-				Query: queryNodeIDs,
+			QuerySequence: []queryAndResult{
+				{
+					Payload: &graphqlQuery{
+						Query: queryNodeIDs,
+					},
+					Expected: `{"data":{"graph":{"nodes":null}}}`,
+				},
 			},
-			Expected: `{"data":{"graph":{"nodes":null}}}`,
 		},
 		{
 			Name: "mutation: login, expect non-existent user",
-			Payload: &struct {
-				Query     string                 `json:"query"`
-				Variables map[string]interface{} `json:"variables"`
-			}{
-				Query:     queryUserLogin,
-				Variables: map[string]interface{}{"auth": map[string]interface{}{"email": "me@ok.com", "password": "ok"}},
+			QuerySequence: []queryAndResult{
+				{
+					Payload: &graphqlQuery{
+						Query:     queryUserLogin,
+						Variables: map[string]interface{}{"auth": map[string]interface{}{"email": "me@ok.com", "password": "ok"}},
+					},
+					Expected: `{"data":{"login":{"success":false,"message":"User does not exist","token":""}}}`,
+				},
 			},
-			Expected: `{"data":{"login":{"success":false,"message":"User does not exist","token":""}}}`,
 		},
 		{
 			Name: "mutation: deleteAccount, expect non-existent user",
-			Payload: &struct {
-				Query     string                 `json:"query"`
-				Variables map[string]interface{} `json:"variables"`
-			}{
-				Query:     mutationDeleteAccount,
-				Variables: map[string]interface{}{"user": "123"},
+			QuerySequence: []queryAndResult{
+				{
+					Payload: &graphqlQuery{
+						Query:     mutationDeleteAccount,
+						Variables: map[string]interface{}{"user": "123"},
+					},
+					Expected: `{"errors":[{"message":"no userID in HTTP-header found","path":["deleteAccount"]}],"data":{"deleteAccount":null}}`,
+				},
 			},
-			Expected: `{"errors":[{"message":"no userID in HTTP-header found","path":["deleteAccount"]}],"data":{"deleteAccount":null}}`,
 		},
-		// FIXME(skep): This test creates a node in the test database, but does not clean up afterwards!
+		// TODO(skep): enable and fix #TDD
 		//{
-		//	Name: "mutation: createNode, expect success",
-		//	Payload: &struct {
-		//		Query     string                 `json:"query"`
-		//		Variables map[string]interface{} `json:"variables"`
-		//	}{
-		//		Query:     mutationCreateNode,
-		//		Variables: map[string]interface{}{"description": map[string]interface{}{"translations": []interface{}{map[string]interface{}{"language": "en", "content": "ok"}}}},
+		//	Name: "mutation: createNode, expect failure, only logged in users may create graph data",
+		//	QuerySequence: []queryAndResult{
+		//		{
+		//			Payload: &graphqlQuery{
+		//				Query:     mutationCreateNode,
+		//				Variables: map[string]interface{}{"description": map[string]interface{}{"translations": []interface{}{map[string]interface{}{"language": "en", "content": "ok"}}}},
+		//			},
+		//			Expected: `{"errors":[{"message":"only logged in user may create graph data","path":["createNode"]}],"data":{"createNode":null}}`,
+		//		},
 		//	},
-		//	Expected: `{"data":{"createNode":{"Status":null}}}`,
+		//},
+		//{
+		//	Name: "flow: create user, 2x create node, create edge, query graph",
+		//	QuerySequence: []queryAndResult{ },
+		//},
+		//{
+		//	Name: "flow: create user, create node, logout, create user, create node, query graph, delete all nodes by user 1 (w/ admin-key)",
+		//	QuerySequence: []queryAndResult{ },
 		//},
 	} {
 		t.Run(test.Name, func(t *testing.T) {
@@ -104,21 +126,23 @@ func TestGraphQLHandlers(t *testing.T) {
 			defer s.Close()
 			c := s.Client()
 			assert := assert.New(t)
-			payload, err := json.Marshal(test.Payload)
-			if !assert.NoError(err) {
-				return
+			for _, testQuery := range test.QuerySequence {
+				payload, err := json.Marshal(testQuery.Payload)
+				if !assert.NoError(err) {
+					return
+				}
+				r, err := c.Post(s.URL, "application/json", strings.NewReader(string(payload)))
+				if !assert.NoError(err) {
+					return
+				}
+				defer r.Body.Close()
+				data, err := io.ReadAll(r.Body)
+				if !assert.NoError(err) {
+					return
+				}
+				got := string(data)
+				assert.Equal(testQuery.Expected, got)
 			}
-			r, err := c.Post(s.URL, "application/json", strings.NewReader(string(payload)))
-			if !assert.NoError(err) {
-				return
-			}
-			defer r.Body.Close()
-			data, err := io.ReadAll(r.Body)
-			if !assert.NoError(err) {
-				return
-			}
-			got := string(data)
-			assert.Equal(test.Expected, got)
 		})
 	}
 }
